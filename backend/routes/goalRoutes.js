00144cfb0198615b6f4ad2e171de505beae7652b
Kevin Mason <krmason2020@gmail.com>
2022-08-04 21:05:03 -0700

    added async handler

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController');
 
-router.get('/', (req, res) => {
-  res.status.json({ message: 'hi' });
-});
+router.get('/', getGoals);
+
+router.post('/', setGoal);
+
+// or what you can do is, but I don't like it as much
+// router.route('/').get('/', getGoals).post('/', setGoal);
+
+router.put('/:id', updateGoal);
+
+router.delete('/:id', deleteGoal);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5001;
 
+const { errorHandler } = require('./middleware/errorMiddleware');
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //to decode body - x-www-form-urlencoded
+// able to recieve req.body.
+
 app.use('/api/goals', require('../backend/routes/goalRoutes'));
+
+app.use(errorHandler);
+//your custom error handler should come last
+
 app.listen(port, () => {
   console.log(`Server Started on port:${port}`);
 });
