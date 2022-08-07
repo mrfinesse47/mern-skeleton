4eda0fc6ff48c749e1dced88a4da82493e2c276c
Kevin Mason <krmason2020@gmail.com>
2022-08-06 23:21:05 -0700

    spinner

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { logout } from '../features/auth/authSlice';
+import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
   return (
     <header className='header'>
       <div className='logo'>
         <Link to='/'>GoalSetter</Link>
       </div>
       <ul>
-        <li>
-          <Link to='/login'>
-            <FaSignInAlt />
-          </Link>
-        </li>
-        <li>
-          <Link to='/register'>
-            <FaUser />
-          </Link>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt />
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser />
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            {/* decided to not have a logout page */}
+            <button
+              className='invisible-button'
+              onClick={() => {
+                dispatch(logout());
+                navigate('/login');
+              }}
+            >
+              <FaSignOutAlt />
+            </button>
+          </li>
+        )}
       </ul>
     </header>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,13 +17,39 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData; //neat trick to not have to have multiple use states
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate('/');
+    }
+    // dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const onSumbit = (e) => {
     e.preventDefault();
+    if (password !== password2) {
+      toast.error('passwords do not match');
+    } else {
+      const userData = { name, email, password };
+      dispatch(register(userData));
+    }
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
@@ -30,7 +61,7 @@ const Register = () => {
       </section>
 
       <section className='form'>
-        <form onSumbit={onSumbit}>
+        <form onSubmit={onSumbit}>
           <div className='form-group'>
             <input
               type='text'
