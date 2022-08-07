b06f1bf731721d19151cc6b952a313d42945f7aa
Kevin Mason <krmason2020@gmail.com>
2022-08-06 20:43:17 -0700

    register and login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData; //neat trick to not have to have multiple use states
+
+  const onChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const onSumbit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Sign In</p>
+      </section>
+
+      <section className='form'>
+        <form onSumbit={onSumbit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter Your Email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter Your password'
+              onChange={onChange}
+            />
+          </div>
+
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,13 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData; //neat trick to not have to have multiple use states
 
-  const onChange = () => {};
+  const onChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const onSumbit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
@@ -24,16 +30,56 @@ const Register = () => {
       </section>
 
       <section className='form'>
-        <form>
-          <input
-            type='text'
-            className='form-control'
-            id='name'
-            name='name'
-            value={name}
-            placeholder='Enter Your Name'
-            onChange={onChange}
-          />
+        <form onSumbit={onSumbit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              value={name}
+              placeholder='Enter Your Name'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter Your Email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter Your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              placeholder='Confirm Your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
         </form>
       </section>
     </>
